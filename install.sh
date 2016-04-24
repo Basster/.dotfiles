ca57dc85178fe4b689106cea5eb0302ad40a88ea
Ole Rößner <o.roessner@neusta.de>
2016-04-24 17:09:22 +0200

    some tuning

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -sv “~/.dotfiles/system/.bashrc” ~
+ln -sv ~/.dotfiles/system/.bashrc ~
 # ln -sv “~/.dotfiles/system/.inputrc” ~
-ln -sv “~/.dotfiles/git/.gitconfig” ~
-ln -sv “~/.dotfiles/git/.gitignore_global” ~
-ln -sv “~/.dotfiles/git/.git-prompt.sh ~
+ln -sv ~/.dotfiles/git/.gitconfig ~
+ln -sv ~/.dotfiles/git/.gitignore_global ~
+ln -sv ~/.dotfiles/git/.git-prompt.sh ~
