08347f9670371fa0c8a2242fef8a34f6de0b6170
Ole Rößner <o.roessner@neusta.de>
2016-04-24 17:24:37 +0200

    vim config added

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -sv ~/.dotfiles/system/.inputrc ~
 ln -sv ~/.dotfiles/git/.gitconfig ~
 ln -sv ~/.dotfiles/git/.gitignore_global ~
 ln -sv ~/.dotfiles/git/.git-prompt.sh ~
+
+ln -sv ~/.dotfiles/vim/.vim ~
+ln -sv ~/.dotfiles/vim/.vimrc ~
