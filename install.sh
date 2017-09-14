990183b9c5108f10ebcd851c958b89cb23f2306c
Ole Rößner <o.roessner@neusta.de>
2017-09-14 10:32:25 +0200

    added .zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -sv ~/.dotfiles/system/.bashrc ~
+ln -sv ~/.dotfiles/system/.zshrc ~
 ln -sv ~/.dotfiles/system/.inputrc ~
 ln -sv ~/.dotfiles/git/.gitconfig ~
 ln -sv ~/.dotfiles/git/.gitignore_global ~
